7d2d6cd3fe74975cd0728cb51a93169b3bf539e9
deylinqff <maycraftminecraft4@gmail.com>
2025-05-07 22:52:28 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const sharp = require('sharp');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    res.status(405).send('Método no permitido');
-    return;
-  }
-
-  try {
-    const buffers = [];
-
-    for await (const chunk of req) {
-      buffers.push(chunk);
-    }
-
-    const imageBuffer = Buffer.concat(buffers);
-    const image = sharp(imageBuffer);
-    const metadata = await image.metadata();
-
-    const width = 1920;
-    const height = Math.round((metadata.height / metadata.width) * width);
-
-    const hdImageBuffer = await image.resize(width, height).jpeg().toBuffer();
-
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.status(200).send(hdImageBuffer);
-  } catch (error) {
-    console.error('Error al procesar la imagen:', error);
-    res.status(500).send('Error al procesar la imagen');
-  }
+module.exports = (req, res) => {
+  res.status(200).json({ mensaje: 'Hola, tu API de prueba está en curso.' });
 };
